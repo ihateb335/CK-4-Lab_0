2bb01a6457e2ea772544e1b832f03229cbc2de6e
ihateb335 <zhar.misha@gmail.com>
2024-09-13 11:45:19 +0300

    Add Lab1

---
+++ src/components/Lab1/panels/ChartPanel/ChartPanel.jsx
@@ -0,0 +1,34 @@
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
+import styles from "./ChartPanel.module.css";
+import DisplayChart from "./DisplayChart/DisplayChart";
+import ChartVisualization from "./ChartVisualization/ChartVisualization";
+
+const ChartPanel = forwardRef( ({
+  data,
+  handleMove,
+  stepSize,
+  maxValue,
+  initialPosition,
+}, ref) => {
+  const visualizationRef = useRef();
+
+  useImperativeHandle(ref, () => ({
+    reset: visualizationRef.current.reset
+  }));
+
+  return (
+    <div className={styles["chart-panel"]}>
+      <DisplayChart data={data} />
+      <ChartVisualization
+        key={data}
+        ref={visualizationRef}
+        handleMove={handleMove}
+        stepSize={stepSize}
+        maxValue={maxValue}
+        initialPosition={initialPosition}
+      />
+    </div>
+  );
+});
+
+export default ChartPanel;
